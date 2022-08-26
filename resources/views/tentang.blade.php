fbfa1899b0afba543fbbfb3afc4294206df06a18
Luiso9 <muhandreop@gmail.com>
2022-08-26 08:56:13 +0700

    first

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,108 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Shyne | Nippah~</title>
+    <meta name="description" content="Shyne's Profile Card">
+    <meta property="og:image" content="https://cdna.artstation.com/p/assets/images/images/034/916/912/original/alexandra-fomina-quanxi-by-moshimoshibe.gif?1613587191">
+    <meta name="theme-color" content="#f54254">
+    
+ <!--  Mod? -->
+    <link rel="icon" type="image/png" sizes="300x250" href="https://media1.tenor.com/images/6863669abb7bb57c71886bc8cbfb274f/tenor.gif?itemid=4884859">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sen">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue" rel="stylesheet">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x">
+</head>
+
+<body style="background-color: var(--background);">
+    <div id="top" style="height: 12px;">
+        <nav class="navbar navbar-light navbar-expand fixed-top" style="color: var(--main-color);border-top-width: 6px;border-top-style: solid;background: var(--background);">
+            <div class="container-fluid"><a class="navbar-brand" href="#" style="color: var(--main-color);font-family: 'Bebas Neue', cursive;">Shyne</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    </div>
+    </nav>
+    </div>
+    <header id="elaina" class="top-round-rainbow d-xl-flex flex-column justify-content-xl-center align-items-xl-center" style="height: 418px;text-align: center;margin: 55px;"><img class="square" src="https://cdnb.artstation.com/p/assets/images/images/031/495/941/original/luka-baric-gif.gif?1603799635" width="150" height="150" style="margin-top: 30px;">
+        <h1 class="d-xl-flex justify-content-xl-start" style="color: var(--text-color);font-family: 'Sen', sans-serif;">Shyne</h1>
+        <p id=outset style="color: var(--secondary-text-color); font-family: 'Noto Sans JP';font-size:10px; "><q>TO HATE OR TO BE HATED, SOMEONE WILL HATE AGAIN. TO SEND OR TO BE SENT TO HELL,<br> SOMEONE WILL BENT THERE AGAIN. I HAVE WITNESSED THIS CYCLE OVER AND OVER AGAIN. NO ONE CAN STOP IT.</q>
+          <br> -Enma Ai</p>
+        <div class="tada animated"><div role="group" class="btn-group">    <a href="https://myanimelist.net/profile/ShyneRo" class="btn btn-primary shadow-none" type="button" style="margin: 5px;background-color: var(--main-color);border-color: var(--main-color);border-radius: 10px;">MyAnimeList</a>
+	
+	<a class="btn btn-primary smoothScroll shadow-none" role="button" style="margin: 5px;background-color: var(--main-color);border-color: var(--main-color);border-radius: 10px;"
+        href="#feature">See More</a></div></div>
+    </header>
+    <div id="feature" style="height: 90px;background: var(--secondary);"><div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="var(--background)" fill-opacity="1" d="M0,96L40,112C80,128,160,160,240,144C320,128,400,64,480,48C560,32,640,64,720,69.3C800,75,880,53,960,58.7C1040,64,1120,96,1200,96C1280,96,1360,64,1400,48L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg></div></div>
+    <div class="features-boxed" style="background: var(--secondary);">
+        <div class="container" style="color: var(--main-color);">
+            <div class="intro">
+                <h2 class="text-center" style="color: var(--text-color);">Social Media</h2>
+            </div>
+                
+            <div class="row justify-content-center features">
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div id="features" class="box" style="/*border-radius: 34px;*/"><i class="fa fa-github icon" style="color: var(--main-color);"></i>
+                        <h3 class="name">Github</h3>
+                        <p class="description" style="color: var(--secondary-text-color);">Luiso9<br></p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div id="features" class="box"><i class="fa fa-user-o icon" style="color: var(--main-color);"></i>
+                        <h3 class="name">Discord</h3>
+                        <p class="description" style="color: var(--secondary-text-color);">andrian#5757<br></p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div id="features" class="box"><i class="fa fa-facebook icon" style="color: var(--main-color);"></i>
+                        <h3 class="name">Facebook</h3>
+                        <p class="description" style="color: var(--secondary-text-color);">Drian<br></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+        <div></div>
+    <div style="background-color: var(--background);"><div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-41.53,-9.56 C167.83,173.98 302.14,-60.89 534.09,89.10 L500.00,0.00 L0.00,0.00 Z" style="stroke: none; fill: var(--secondary);"></path></svg></div></div>
+    
+    
+    
+    <div class="team-clean" style="background: rgba(0,0,0,0);">
+        <div class="container" style="filter: blur(0px);padding-bottom: 0px;">
+            <h3 class="text-center name" style="color: var(--secondary-text-color)">Favorite Anime!</h3>
+            <div class="intro"></div>
+            <div class="row row-cols-4 d-flex d-xl-flex justify-content-center justify-content-xl-center people" style="padding-bottom: 0px;">
+                <div class="col-auto col-md-6 col-lg-4 yeet"><a href="https://myanimelist.net/anime/5081/Bakemonogatari" target="_blank"><img class="rounded-circle partner" src="https://media.discordapp.net/attachments/809988356611112980/853342184307687454/unknown.jpeg" alt="Bakemonogatari"></a></div>
+                <div class="col-auto col-md-6 col-lg-4 yeet"><a href="https://myanimelist.net/anime/4181/Clannad__After_Story" target="_blank"><img class="rounded-circle partner" src="https://i0.wp.com/livedoor.blogimg.jp/omurin/imgs/e/8/e86e19f2.jpg?w=600" alt="Clannad : After Story"></a></div>
+                <div class="col-auto col-md-6 col-lg-4 yeet"><a href="https://discord.gg/" target="_blank"><img class="rounded-circle partner" src="https://media.discordapp.net/attachments/809988356611112980/853341717343764520/images.png" alt="K-On"></a></div>
+            </div>
+        </div>
+    </div>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="var(--secondary)" fill-opacity="1" d="M0,160L48,154.7C96,149,192,139,288,154.7C384,171,480,213,576,218.7C672,224,768,192,864,181.3C960,171,1056,181,1152,192C1248,203,1344,213,1392,218.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    <div class="footer-dark" style="background: var(--secondary);padding-top: 0px;padding-bottom: 0px;">
+        <div class="container">
+            <div class="row">
+                <div class="col item social"><a class="smoothScroll" href="#top"><i class="icon ion-android-arrow-up"></i></a></div>
+            </div>
+            <p class="copyright" style="color: var(--text-color);">Website © 2021</p>
+            <p class="copyright" style="color: var(--text-color);padding: 0px;">
+                <!--Consider leaving this to give credit to me for making this, I worked hard on it and it makes me feel good :D-->Theme by&nbsp;<a href="https://loli.drian-got-drip.tk" style="color: var(--main-color);">Shyne</a></p>
+        </div>
+        </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="assets/js/untitled.js"></script>
+    <script src="https://kit.fontawesome.com/60f4a26759.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
